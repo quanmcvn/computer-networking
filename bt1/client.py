4b8183b74b9ca94c69210389bad45f27a692eb1c
danquan <dangquanattvhouse@gmail.com>
2024-03-20 01:14:23 +0700

    simple game

--- bt1/client.py
+++ bt1/client.py
@@ -2,17 +2,23 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "192.168.92.136" # hardcoded local ip
-
-port = 10004
-
+host = "192.168.1.169" # hardcoded server's ip
+port = 1105
 client_socket.connect((host, port))
 
+print("GUESSING GAME")
+print("Enter 'END GAME' to quit the game")
+print("Enter 'NEW GAME' to start a new game")
+
 while True:
-	msg = input()
+	msg = input("Please enter a guess, or a command: ")
 	client_socket.send(msg.encode('ascii'))
-	print(client_socket.recv(1024).decode('ascii'))
-	if (msg == "quit"):
+
+	status_code = client_socket.recv(1024).decode('ascii')
+	msg_back = client_socket.recv(1024).decode('ascii')
+	print(msg_back)
+
+	if (status_code == "1"):
 		break
 
 client_socket.close()
--- bt1/server.py
+++ bt1/server.py
@@ -1,26 +1,52 @@
 import socket
+import random
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-host = "192.168.92.136" # hardcoded local ip
-
-port = 10004
-
+host = socket.gethostname()
+port = 1105
 server_socket.bind((host, port))
 
 server_socket.listen(1)
 
 client_socket, addr = server_socket.accept()
 
+x = random.randint(1, 100)
+
 while True:
 	msg = client_socket.recv(1024).decode('ascii')
-	if (msg == "quit"):
-		client_socket.close()
-		break
-	msg = msg.upper()
-	print(msg)
-	client_socket.send(msg.encode('ascii'))
 
+	print("Received message from client:", msg)
+
+	msg = msg.upper()
+	print("Edited message from client:", msg)
+	# quit game
+	if (msg == "END GAME"):
+		client_socket.send("1".encode('ascii'))
+		client_socket.send("GOOD BYE, SEE YOU AGAIN !".encode('ascii'))
+		break
+	elif (msg == "NEW GAME"):
+		x = random.randint(1, 100)
+		client_socket.send("2".encode('ascii'))
+		client_socket.send("New game started!!!".encode('ascii'))
+		continue
+
+	# check if the message is a number
+	try:
+		guess = int(msg)
+		client_socket.send("3".encode('ascii'))
+		if (guess == x):
+			client_socket.send("Correct!!!\nNew game started!!!".encode('ascii'))
+		elif (guess < x):
+			client_socket.send("Too low!!!".encode('ascii'))
+		else:
+			client_socket.send("Too high!!!".encode('ascii'))
+	except:
+		client_socket.send("4".encode('ascii'))
+		client_socket.send("Please enter a number!!!".encode('ascii'))
+		continue
+
+client_socket.close()
 server_socket.close()
